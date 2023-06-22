5734d4f977a1208741588abca1683c2aea3d67e5
Elias <eliasjuniorfranca@hotmail.com>
2023-06-21 22:53:51 -0300

    alteracao do Menor valor HTML JS

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,7 +1,12 @@
+const menorValor = 10 
+const maiorValor= 100
 const numeroSecreto = gerarNumeroAleatorio()
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 100)
+    return parseInt(Math.random() * maiorValor+1)
 }   
 
 console.log('Numero secreto:',numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
