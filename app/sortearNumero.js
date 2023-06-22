750863b5dfb45d259f2c12de1970394f4cbe9f59
Elias <eliasjuniorfranca@hotmail.com>
2023-06-21 22:54:45 -0300

    alt maior valor HTML JS

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,5 +1,5 @@
 const menorValor = 10 
-const maiorValor= 100
+const maiorValor= 1000
 const numeroSecreto = gerarNumeroAleatorio()
 
 function gerarNumeroAleatorio(){
@@ -10,3 +10,6 @@ console.log('Numero secreto:',numeroSecreto)
 
 const elementoMenorValor = document.getElementById('menor-valor')
 elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
