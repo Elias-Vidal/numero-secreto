16869086be0a4d4f82442a9feb33ce2cd386709e
Elias <eliasjuniorfranca@hotmail.com>
2023-06-24 17:40:07 -0300

    Cod Funciona com lentidao, verificar

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -2,7 +2,7 @@ const elementoChute = document.getElementById('chute')
 
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
-const recognition = new webkitSpeechRecognition();
+const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
 
