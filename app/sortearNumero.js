ec9639e827373af24d28e272e271686a7e77ead2
Elias <eliasjuniorfranca@hotmail.com>
2023-06-24 11:40:09 -0300

    alteracoes para testes

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,12 +1,12 @@
-const menorValor = 10 
-const maiorValor= 1000
+const menorValor = 1
+const maiorValor = 1000
 const numeroSecreto = gerarNumeroAleatorio()
 
-function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * maiorValor+1)
-}   
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
 
-console.log('Numero secreto:',numeroSecreto)
+console.log('Número Secreto:', numeroSecreto)
 
 const elementoMenorValor = document.getElementById('menor-valor')
 elementoMenorValor.innerHTML = menorValor
