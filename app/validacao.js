e3f7a1dade1a6ffb16c000ccb7df6e602db77528
Elias <eliasjuniorfranca@hotmail.com>
2023-06-27 22:30:26 -0300

    COD

--- app/validacao.js
+++ app/validacao.js
@@ -1,8 +1,12 @@
 function VerificaSeOChutePossuiUmValorValido(chute){
     const numero = +chute
 
-    if(Number.isNaN(numero)){
+    if(chuteForInvalido(numero)){
         console.log('Valor invalido')
     }
     
+}
+
+function newFunction(numero) {
+    return Number.isNaN(numero)
 }
