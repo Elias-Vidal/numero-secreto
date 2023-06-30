ea6ce08c155d6d531dd3d90b5e6288bf757ee5a8
Elias <eliasjuniorfranca@hotmail.com>
2023-06-29 23:34:32 -0300

    voz

--- app/validacao.js
+++ app/validacao.js
@@ -22,7 +22,9 @@ function VerificaSeOChutePossuiUmValorValido(chute){
         <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
     }else {
-        
+        elementoChute.innerHTML +=`
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
     }
 
     }
